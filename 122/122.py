7a50505a2263a8eb775d2a0cb4e0944668e2a50c
danmedani <danmedani@gmail.com>
2015-08-30 02:39:07 -0700

    speeding it up a bit

--- 122/122.py
+++ 122/122.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python
-import copy
+import copy, random
 
 MAX = 10 ** 5
 a = 1
-b = 25
+b = 50
 mappy = {}
 
 def hashIt(numberBag, exponent):
 	return reduce(lambda x, y : x + '|' +  y, map(str, sorted(numberBag))) + str(exponent)
 
+lowBall = MAX
 def iterate(numberBag, exponent):
+	global lowBall
+
 	hVal = hashIt(numberBag, exponent)
 	if (hVal in mappy):
 		return mappy[hVal]
 
+	numMults = len(numberBag) - 1
+
+	if (numMults > lowBall):
+		return MAX
+
 	if (exponent in numberBag):
 		# o, we found it
-		return len(numberBag) - 1
+		if (numMults < lowBall):
+			lowBall = numMults
+		return numMults
 
 	# gather all possible branches
 	branches = []
@@ -32,7 +42,7 @@ def iterate(numberBag, exponent):
 
 	# find min for each branch
 	minimum = MAX
-	for branch in branches:
+	for branch in reversed(sorted(branches)):
 		newNumberBag = copy.deepcopy(numberBag)
 		newNumberBag.add(branch)
 		minVal = iterate(newNumberBag, exponent)
@@ -40,10 +50,15 @@ def iterate(numberBag, exponent):
 			minimum = minVal
 
 	mappy[hVal] = minimum
+	if (minimum < lowBall):
+		lowBall = minimum
 	return minimum
 
 def getMinMult(n):
+	global lowBall, mappy
+
 	mappy = {}
+	lowBall = MAX
 	return iterate(set([1]), n)
 
 def getSumMinMult(a, b):
