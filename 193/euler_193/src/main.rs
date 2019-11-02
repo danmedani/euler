3140ca30972d5e086ff45a4d3f03116e19ec06e8
Dan Medani <danmedani@gmail.com>
2019-11-01 23:12:49 -0700

    a bit closer, i bet

--- 193/euler_193/src/main.rs
+++ 193/euler_193/src/main.rs
@@ -1,15 +1,58 @@
 use prime_tools;
 
-// fn find_
+fn count_pairings(
+	current_value: u64,
+	top_val: u64, 
+	prime_squares: &Vec<u64>, 
+	min_index: usize, 
+	choices_left: u8,
+	add: bool
+) {
+	if choices_left == 0 {
+		return top_val / current_value;
+	}
+
+	let mut current_index = min_index;
+	while current_index < prime_squares.len() {
+		let mut total = 0;
+		match current_value.checked_mul(prime_squares[current_index]) {
+			None => break,
+			Some(val) => {
+				total = val;
+			}
+		}
+
+		total += count_pairings(
+			total,
+			top_val,
+			prime_squares,
+			current_index + 1,
+			choices_left - 1,
+			!add
+		);
+	}
+
+	total
+}
 
 fn main() {
     println!("Hello, world!");
     // 33_554_432 == sqrt(2^50)
     // let max_val = 100;
     // let max_val = 2u64.pow(50);
-    let prime_squares = get_squared_primes(100);
-    
+    let max_val = 100;
+    let prime_squares = get_squared_primes(max_val);
+
+    let top_val = max_val - 1;
     println!("{:?}", prime_squares);
+
+    let mut total_count = top_val;
+    for squarey in prime_squares.iter() {
+    	let squarey_count = top_val / squarey;
+    	total_count = total_count - squarey_count;
+    }
+
+    println!("total = {}", total_count);
 }
 
 
