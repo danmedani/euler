9ad8ddf5d019ec25444a9d76ef693838c1b4a927
Dan <danmedani@gmail.com>
2020-10-03 20:28:51 -0700

    ugly but it works

--- 126/126.py
+++ 126/126.py
@@ -31,14 +31,41 @@ def get_neighbors(cube: Tuple[int, int, int]) -> List[Tuple[int, int, int]]:
 	]
 
 
-def generate_size_sequence(num_steps: int, starting_layer: List[Tuple[int, int, int]]) -> List[int]:
+def generate_size_sequence(max_num: int, starting_layer: List[Tuple[int, int, int]]) -> List[int]:
 	sequence: List[int] = []
 	all_cubes = set()
 	for cube in starting_layer:
 		all_cubes.add(encode(cube))
 	last_layer: List[Tuple[int, int, int]] = starting_layer
 
-	for i in range(num_steps):
+
+	while len(last_layer) < max_num:
+		this_layer: List[Tuple[int, int, int]] = []
+		layer_count = 0
+		for cube in last_layer:
+			neighbors = get_neighbors(cube)
+			for neighbor in neighbors:
+				encoded_neighbor = encode(neighbor)
+				if encoded_neighbor not in all_cubes:
+					this_layer.append(neighbor)
+					all_cubes.add(encoded_neighbor)
+					layer_count += 1
+		
+		sequence.append(layer_count)
+		last_layer = this_layer
+	return sequence
+
+
+
+def generate_size_sequence_with_steps(max_steps: int, starting_layer: List[Tuple[int, int, int]]) -> List[int]:
+	sequence: List[int] = []
+	all_cubes = set()
+	for cube in starting_layer:
+		all_cubes.add(encode(cube))
+	last_layer: List[Tuple[int, int, int]] = starting_layer
+
+
+	for i in range(max_steps):
 		this_layer: List[Tuple[int, int, int]] = []
 		layer_count = 0
 		for cube in last_layer:
@@ -63,28 +90,24 @@ def generate_starting_layer(x_size: int, y_size: int, z_size: int) -> List[Tuple
 				layer.append((x, y, z))
 	return layer
 
-# seq = generate_size_sequence(10, generate_starting_layer(8, 8, 8))
-# seq = generate_size_sequence(50, generate_starting_layer(2, 3, 1))
 
-# print(seq[:5])
-# diff_seq = []
-# for i in range(len(seq) - 1):
-# 	diff = seq[i+1] - seq[i]
-# 	diff_seq.append(diff)
-# 	# print(seq[i], diff)
-
-# for i in range(len(seq) - 2):
-# 	print(seq[i], diff_seq[i], diff_seq[i + 1] - diff_seq[i])
-
+def gen_slow(x: int, y: int, z: int, max_num: int) -> List[int]:
+	return generate_size_sequence(max_num, generate_starting_layer(x, y, z))[:-1]
 
 def gen_fast(x: int, y: int, z: int, max_num: int) -> List[int]:
-	if min([x, y, z]) > 2:
-		first = (x * y * z) - ((x - 2) * (y - 2) * (z - 2))
-	else:
-		first = x * y * z
+	# if x > 2:
+	# 	first = (x * y * z) - ((x - 2) * (y - 2) * (z - 2))
+	# else:
+	# 	# if x == 1:
+	# 	# 	seq = generate_size_sequence_with_steps(2, generate_starting_layer(x, y, z))
+	# 	# 	first = seq[0] - (seq[1] - seq[0] - 8)
+	# 	# else:
+	# 	first = x * y * z
 
 	layer_length = 2 * ((x*y) + (x*z) + (y*z))
-	delta = layer_length - first
+	second_layer_length = layer_length + (4 * (x + y + z))
+	delta = second_layer_length - layer_length - 8
+	
 	sequence = []
 	while layer_length < max_num:
 		sequence.append(layer_length)
@@ -92,41 +115,53 @@ def gen_fast(x: int, y: int, z: int, max_num: int) -> List[int]:
 		layer_length += delta
 	return sequence
 
-print(gen_fast(11, 1, 1, 50))
+# print(gen_slow(6, 12, 14, 2000))
+# print(gen_fast(6, 12, 14, 2000))
 
-# MAX = 1000
-# MAX_DIM = 1000
+MAX = 20000
+MAX_DIM = MAX
 
-# count_map = {}
-# for x in range(1, MAX_DIM):
-# 	for y in range(x, MAX_DIM):
-# 		y_break = False
-# 		for z in range(y, MAX_DIM):
-# 			seq = gen_fast(x, y, z, MAX)
-# 			if len(seq) > 0:
-# 				for val in seq:
-# 					if val not in count_map:
-# 						count_map[val] = 0
-# 					count_map[val] += 1
-# 			else:
-# 				if z > 1:
-# 					# z too big - z is the cause. break out of z, go to next y
-# 					break 
-# 				else:
-# 					# z not the problem. x or y are the problem
-# 					if y > 1:
-# 						# y too big. let's break out of z (and y!)
-# 						y_break = True
-# 						break
-# 		if y_break:
-# 			break
+count_map = {}
+for x in range(1, MAX_DIM):
+	print(x)
+	for y in range(x, MAX_DIM):
+		y_break = False
+		for z in range(y, MAX_DIM):
+			# if gen_slow(x, y, z, MAX) != gen_fast(x, y, z, MAX):
+			# 	print('o nooooo', x, y, z)
+			# 	print(gen_slow(x, y, z, MAX))
+			# 	print(gen_fast(x, y, z, MAX))
+			# 	print()
 
 
+			seq = gen_fast(x, y, z, MAX)
+			if len(seq) > 0:
+				for val in seq:
+					if val not in count_map:
+						count_map[val] = 0
+					count_map[val] += 1
 
-# ordered_keys = sorted(count_map.keys())
+			else:
+				if z > 1:
+					# z too big - z is the cause. break out of z, go to next y
+					break 
+				else:
+					# z not the problem. x or y are the problem
+					if y > 1:
+						# y too big. let's break out of z (and y!)
+						y_break = True
+						break
+		if y_break:
+			break
 
-# for key in ordered_keys:
-# 	print(key, count_map[key])
+print('hi')
+
+ordered_keys = sorted(count_map.keys())
+
+for key in ordered_keys:
+	if count_map[key] == 1000:
+		print('found one', key, count_map[key])
+	# print(key, count_map[key])
 
 
 
