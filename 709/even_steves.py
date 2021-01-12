494f89fd167e2a92ce1aa0ee76366f59b4b9ec71
Dan <dmedani@yelp.com>
2021-01-11 22:47:35 -0800

    unpacker

--- 709/even_steves.py
+++ 709/even_steves.py
@@ -29,34 +29,60 @@ from typing import List
 def pack(bag: int, cabinet: Dict[int, List[int]]) -> str:
 	packing = []
 	for a_bag in cabinet[bag]:
-		packing.append(str(a_bag))
+		packing.append(str(a_bag) + '|')
 		if a_bag in cabinet:
 			packing.append('(')
 			packing.append(pack(a_bag, cabinet))
 			packing.append(')')
 	return ''.join(packing)
 
-print(pack(0, {
+
+def unpack(packing: str) -> Dict[int, List[int]]:
+	cabinet = {}
+	bag_stack = [
+		(0, 0, len(packing))
+	]
+
+	while bag_stack:
+		current_bag, start, end = bag_stack.pop()
+		cabinet[current_bag] = []
+		index = start
+		while index < end:
+			if packing[index] != '(':
+				if index == len(packing) - 1 and packing[index] == ')':
+					index += 1
+					continue
+
+				number_end_index = index + 1
+				while number_end_index < len(packing) and packing[number_end_index] != '|':
+					number_end_index += 1
+				cabinet[current_bag].append(int(packing[index:number_end_index]))
+				index = number_end_index + 1
+			elif packing[index] == '(':
+				layers_deep = 0
+				inner_bag_end = index + 1
+				while packing[inner_bag_end] != ')' or layers_deep > 0:
+					inner_bag_end += 1
+
+					if packing[inner_bag_end] == '(':
+						layers_deep += 1
+					elif packing[inner_bag_end] == ')':
+						layers_deep -=1
+				bag_stack.append((cabinet[current_bag][-1], index + 1, inner_bag_end - 1))
+				
+				index = inner_bag_end + 1
+			else:
+				raise Exception('NOT SUPPOSED TO BE HERE!')
+
+	return cabinet
+
+
+print(pack(0, unpack(pack(0, {
 	0: [1, 2, 3],
-	1: [4, 5],
-	5: [6, 7]	
-}))
-
-# def unpack(packing: str) -> Dict[int, List[int]]:
-# 	cabinet = {}
-# 	current_bag = 0
-# 	bag_stack = []
-# 	for index in range(len(packing)):
-# 		if packing[index] not in ['(', ')']:
-# 			cabinet[current_bag].append(int(packing[index]))
-# 		elif packing[index] == '(':
-# 			bag_stack.append(current_bag)
-# 			current_bag = packing[index]
-			
-
-
-
-
+	3: [4, 5],
+	5: [6, 7],
+	7: [11, 12, 13]
+}))))
 
 # def even_steven(cabinet: Dict[int, List[int]], num_bags_to_take: int) -> List[Dict[int, List[int]]]:
 # 	return [
